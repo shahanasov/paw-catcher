ffbfa95b22a69ceba67bce9a128e69d218426bf4
Shahanas <shahanasov@gmail.com>
2025-02-12 09:39:26 +0530

    first commit

---
+++ lib/data/services/auth_service.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+class AuthService {
+  final FirebaseAuth auth=FirebaseAuth.instance;
+
+  Future<void> signInWithEmail({
+    required String email,
+    required String password
+  })async{
+    await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  
+  signOut() async {
+    await auth.signOut();
+  }
+  
+}
---
+++ lib/presentation/auth/widgets/widgets.dart
@@ -0,0 +1,41 @@
+import 'package:dog_catcher/core/theme.dart';
+import 'package:dog_catcher/presentation/auth/sign_in.dart';
+import 'package:dog_catcher/presentation/auth/sign_up.dart';
+import 'package:flutter/material.dart';
+
+Widget textfield(
+    {required TextEditingController controller, String? hint, String? label}) {
+  return TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+      hintStyle: TextStyle(color: AppTheme().textSecondary),
+      hintText: hint ?? 'Type here',
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15), borderSide: BorderSide.none),
+      filled: true,
+      fillColor: AppTheme().softPink,
+    ),
+  );
+}
+
+Widget optsign(BuildContext context, bool have) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(have ? "Don't have an account?" : "Have an account?"),
+      TextButton(
+        child: Text(
+          have ? 'Sign Up' : "Sign In",
+          style: TextStyle(color: AppTheme().softPink),
+        ),
+        onPressed: () {
+          have
+              ? Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => SignUpScreen()))
+              : Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => SignInScreen()));
+        },
+      )
+    ],
+  );
+}
